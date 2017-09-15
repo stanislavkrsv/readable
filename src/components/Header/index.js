5892c072d698082c88cce981b1fe5abcf1280f3e
Stanislav Karassyov <stanislav.krsv@gmail.com>
2017-09-16 01:56:55 +0600

    refactor: Header converted to stateless function components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,24 +1,19 @@
-import React, { Component } from 'react'
-import {NavLink} from 'react-router-dom'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 /**
  * Header
  */
-class Header extends Component {
-  render(){
-    return(
-      <header className="header">
-        <div className="header__wrapper">
-          <div className="header__logo">
-            <NavLink to="/" exact>Readable</NavLink>
-          </div>
-          <div className="header__button">
-            <NavLink to="/new" className="btn btn--action">Create Post</NavLink>
-          </div>
-        </div>
-      </header>
-    )
-  }
-}
+const Header = () =>
+  <header className="header">
+    <div className="header__wrapper">
+      <div className="header__logo">
+        <NavLink to="/" exact>Readable</NavLink>
+      </div>
+      <div className="header__button">
+        <NavLink to="/new" className="btn btn--action">Create Post</NavLink>
+      </div>
+    </div>
+  </header>
 
 export default Header
