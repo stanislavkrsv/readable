a8d6b0c12522d7dffa7d7431ada2226ca429e32f
Stanislav Karassyov <stanislav.krsv@gmail.com>
2017-09-15 12:40:16 +0600

    Added 404 page

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/* 404 page */
+const NotFound = () =>
+  <div className="page404">
+    <h3 className="page404__title">404 page not found :(</h3>
+    <p className="page404__text">We are sorry but the page you are looking for does not exist.</p>
+  </div>
+
+export default NotFound;
